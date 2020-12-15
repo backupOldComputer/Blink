1745a3163e6390893aabb09b243e95c2f888ce63
钟本聪 <641045726@qq.com>
2020-12-15 20:16:36 +0800

    递归写入先亮后暗

--- src/MS2Frame.java
+++ src/MS2Frame.java
@@ -5,7 +5,7 @@ import java.awt.image.*;
 import javax.imageio.*;
 
 public class MS2Frame{
-    public static final OutputStream sout = System.out;
+    public static final OutputStream sout = System.out;	//new FileOutputStream();	//
     public static final String FRAME_FORMAT = "PNG"; //我的ffmpeg不支持tiff
     public static final int HALF_HDP = 8;
     public static String s2m(String path){
@@ -16,41 +16,32 @@ public class MS2Frame{
 	for(int pictureIndex=0;pictureIndex<args.length;++pictureIndex){
 		String sPath = args[pictureIndex];
 		String mPath = s2m(args[pictureIndex]);
-		BufferedImage[] frames = new BufferedImage[2*HALF_HDP];
-		frames[0] = ImageIO.read(new FileInputStream(mPath));
+		BufferedImage m = ImageIO.read(new FileInputStream(mPath));
 		BufferedImage s = ImageIO.read(new FileInputStream(sPath));
-		if(frames[0].getWidth()!=s.getWidth() || frames[0].getHeight()!=s.getHeight())
-			throw new AssertionError();
-	        for(int frameIndex=1; frameIndex<=HALF_HDP; ++frameIndex){
-			frames[frameIndex] = ImageIO.read(new FileInputStream(mPath));
-			computeFrame(frames[frameIndex], s, frameIndex);
-			//TODO:及时把输出流传给ffmpeg
-			frames[2*HALF_HDP-frameIndex] = frames[frameIndex];
-		}
-		for(int k=0;k<frames.length;++k)
-			ImageIO.write(frames[k], FRAME_FORMAT, System.out);
-		}
+		pictureToFrames(m, s);
+	}
     }
-    /** 写入第frameIndex帧到第(2*HALF_HDP-frameIndex)帧
-    */
-    public static void recurWrite(BufferedImage m, final BufferedImage s, int frameIndex) throws Exception {
-	if(frameIndex < 0 || frameIndex >= HALF_HDP)
+    public static void pictureToFrames(BufferedImage m, final BufferedImage s) throws Exception {
+	if(m.getWidth()!=s.getWidth() || m.getHeight()!=s.getHeight())
+		throw new AssertionError();
+	//检查宽与高后用frameIndex=0启动递归写入
+	recurWrite(m, s, 0);
+    }
+    /** 写入第frameIndex帧到第(2*HALF_HDP-frameIndex)帧 */
+    public static void recurWrite(BufferedImage m, final BufferedImage s, final int frameIndex) throws Exception {
+	if(frameIndex < 0 || frameIndex > HALF_HDP)
 		throw new AssertionError();
 	//写入第frameIndex帧
 	ImageIO.write(m, FRAME_FORMAT, sout);
 	boolean moreBrighter = true;
 	nextFrame(m,s,moreBrighter);
-	//写入中间帧
-	++frameIndex;
+	//递归写入中间帧
 	if(frameIndex == HALF_HDP) return; //frameIndex自增到了中间就不再展开新递归
-	recurWrite(m,s,frameIndex);
+	recurWrite(m,s,1+frameIndex);
 	//写入第(2*HALF_HDP-frameIndex)帧
 	ImageIO.write(m, FRAME_FORMAT, sout);
 	moreBrighter = false;
 	nextFrame(m,s,moreBrighter);
-    }
-    public static BufferedImage cloneBufferedImage(final BufferedImage from){
-	return null;//TODO
     }
     public static void nextFrame(BufferedImage m, final BufferedImage s, final boolean moreBrighter){
 	for(int i=0; i<s.getWidth(); ++i){
@@ -60,16 +51,6 @@ public class MS2Frame{
 			Color source = new Color(m.getRGB(i,j), true);
 			Color dest = moreBrighter ? source.brighter() : source.darker();
 			m.setRGB(i,j,dest.getRGB());
-		}
-	}
-    }
-    public static void computeFrame(BufferedImage dest, final BufferedImage s, final int frameIndex){
-	for(int i=0; i<s.getWidth(); ++i){
-		for(int j=0; j<s.getHeight(); ++j){
-			if(s.getRGB(i,j) != Color.WHITE.getRGB())
-				continue;
-			int source = dest.getRGB(i,j);
-			dest.setRGB(i,j,source+0x80000*frameIndex);//new Color(source, true).brighter().getRGB());
 		}
 	}
     }

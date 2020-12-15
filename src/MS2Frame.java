355d78a16320fc59a76c7aa7036882f308f434f1
钟本聪 <641045726@qq.com>
2020-12-15 16:38:04 +0800

    细节重构

--- src/MS2Frame.java
+++ src/MS2Frame.java
@@ -18,14 +18,8 @@ public class MS2Frame{
 			throw new AssertionError();
 	        for(int frameIndex=1; frameIndex<=HALF_HDP; ++frameIndex){
 			frames[frameIndex] = ImageIO.read(new FileInputStream(mPath));
-			for(int i=0; i<s.getWidth(); ++i){
-				for(int j=0; j<s.getHeight(); ++j){
-					if(s.getRGB(i,j) != Color.WHITE.getRGB())
-						continue;
-					int source = frames[frameIndex].getRGB(i,j);
-					frames[frameIndex].setRGB(i,j,source+0x80000*frameIndex);//new Color(source, true).brighter().getRGB());
-				}
-			}
+			computeFrame(frames[frameIndex], s, frameIndex);
+			//TODO:及时把输出流传给ffmpeg
 			frames[2*HALF_HDP-frameIndex] = frames[frameIndex];
 		}
 		for(int k=0;k<frames.length;++k)
@@ -36,38 +30,14 @@ public class MS2Frame{
 	return path.substring(0, path.length() - 4 );
 	//return path.replaceFirst("pWarehouse/S", "pWarehouse/M");
     }
-/*
-    public static void ms2Frame(BufferedImage m, BufferedImage s) throws Exception {
-	if( m.getWidth() != s.getWidth() || m.getHeight() != s.getHeight())
-		throw new AssertionError();
-	BufferedImage[] frames = new BufferedImage[2*HALF_HDP];//每张图生成(HALF_HDP*2)帧
-//	frames[0] = m.clone();
-        for(int frameIndex=1; frameIndex<=HALF_HDP; ++frameIndex){
-		frames[frameIndex] = ImageIO.read(new FileInputStream(mPath));
-		for(int i=0; i<s.getWidth(); ++i){
-			for(int j=0; j<s.getHeight(); ++j){
-				if(s.getRGB(i,j) != Color.WHITE.getRGB())
-					continue;
-				int source = m.getRGB(i,j);
-				m.setRGB(source+0x20000);//i,j,new Color(source, true).brighter().getRGB());
-			}
+    public static void computeFrame(BufferedImage dest, BufferedImage s, int frameIndex){
+	for(int i=0; i<s.getWidth(); ++i){
+		for(int j=0; j<s.getHeight(); ++j){
+			if(s.getRGB(i,j) != Color.WHITE.getRGB())
+				continue;
+			int source = dest.getRGB(i,j);
+			dest.setRGB(i,j,source+0x80000*frameIndex);//new Color(source, true).brighter().getRGB());
 		}
-		frames[2*HALF_HDP-frameIndex] = frames[i];
-	//	ImageIO.write(m, FRAME_FORMAT, System.out);
 	}
-        for(int frameIndex=1; frameIndex<=HALF_HDP; ++frameIndex){
-		for(int i=0; i<m.getWidth(); ++i){
-			for(int j=0; j<m.getHeight(); ++j){
-				if(s.getRGB(i,j) != Color.WHITE.getRGB())
-					continue;
-				int source = m.getRGB(i,j);
-				m.setRGB(i,j,new Color(source, true).darker().getRGB());
-			}
-		}
-		ImageIO.write(m, FRAME_FORMAT, System.out);
-	}
-	
-	for(int k=0;k<frames.length;++k)
-		ImageIO.write(m, FRAME_FORMAT, System.out);
-    }*/
+    }
 }

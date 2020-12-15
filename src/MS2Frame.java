a292aa4618ad2852d53839059ecd7fa0911f467d
钟本聪 <641045726@qq.com>
2020-12-15 19:12:35 +0800

    开始研究BufferedImage的拷贝

--- src/MS2Frame.java
+++ src/MS2Frame.java
@@ -7,6 +7,10 @@ import javax.imageio.*;
 public class MS2Frame{
     public static final String FRAME_FORMAT = "PNG"; //我的ffmpeg不支持tiff
     public static final int HALF_HDP = 8;
+    public static String s2m(String path){
+	return path.substring(0, path.length() - 4 );
+	//return path.replaceFirst("pWarehouse/S", "pWarehouse/M");
+    }
     public static void main(String[] args) throws Exception {
 	for(int pictureIndex=0;pictureIndex<args.length;++pictureIndex){
 		String sPath = args[pictureIndex];
@@ -26,11 +30,10 @@ public class MS2Frame{
 			ImageIO.write(frames[k], FRAME_FORMAT, System.out);
 		}
     }
-    public static String s2m(String path){
-	return path.substring(0, path.length() - 4 );
-	//return path.replaceFirst("pWarehouse/S", "pWarehouse/M");
+    public static BufferedImage cloneBufferedImage(final BufferedImage from){
+	return null;//TODO
     }
-    public static void computeFrame(BufferedImage dest, BufferedImage s, int frameIndex){
+    public static void computeFrame(BufferedImage dest, final BufferedImage s, final int frameIndex){
 	for(int i=0; i<s.getWidth(); ++i){
 		for(int j=0; j<s.getHeight(); ++j){
 			if(s.getRGB(i,j) != Color.WHITE.getRGB())

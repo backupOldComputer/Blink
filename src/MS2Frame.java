d962669d6ae5bf13609d629aada2f40f01a55393
钟本聪 <641045726@qq.com>
2020-12-20 17:59:53 +0800

    重构+注释

--- src/MS2Frame.java
+++ src/MS2Frame.java
@@ -47,30 +47,33 @@ public class MS2Frame{
 		pictureToFrames(m, s);
 	}
     }
-    public static boolean isChooes(int rgb){
+    public static boolean isChooes(int rgb) {
 	return rgb == Color.WHITE.getRGB();
     }
-    public static int rgba2Red(int rgba){
+    public static int rgba2Red(int rgba) {
 	return ( rgba/0x10000 ) & 0xff;
+    }
+    public static BufferedImage imClone(BufferedImage from) {
+	BufferedImage to = new BufferedImage(from.getWidth(), from.getHeight(), from.getType());
+        to.setData(from.getData());
+	return to;
     }
     public static void pictureToFrames(BufferedImage m, BufferedImage s) throws IOException {
 	int r = s.getWidth();
 	int c = s.getHeight();
 	if( r > m.getWidth() || c > m.getHeight() ) throw new AssertionError();
-	int rb=-1,re=0,cb=c-1,ce=0;
+	int rb=-1,re=0,cb=(c-1),ce=0;	//二维for循环计算 选区 窗口 矩阵 坐标
 	for(int i=0; i<r; ++i){	//TODO:Raster与起始坐标优化
 	    for(int j=0; j<c; ++j){
 		if( isChooes( s.getRGB(i,j) ) ){
-		    //首个选区像素可确定rb，最后一个选区像素可确定re
-		    if(rb == -1) rb = i; 
-		    re = i; 
+		    if(rb == -1) rb = i; // 首个选区像素可确定rb
+		    re = i; // 最后一个选区像素可确定re
 		    cb = (j<cb)?j:cb;
 		    ce = (j>ce)?j:ce; 
 	   	}
 	    }
 	}
-	//DOING:minX和minY优化性能
-	int[][] step = new int[re+1-rb][ce+1-cb];
+	int[][] step = new int[re+1-rb][ce+1-cb]; //三重循环处理选区红亮
 	for(int i=0; i < step.length ; ++i){
 		int x = i+rb;
 		for(int j=0; j < step[0].length ; ++j){
@@ -79,30 +82,41 @@ public class MS2Frame{
 				step[i][j] = 0;//选区外为0，选区内为step值
 				continue;
 			}
-			int red;//处理过亮的像素
-			while( ( red = rgba2Red(m.getRGB(x,y)) ) > MAX_SUB_RED){
-				Color source = new Color(m.getRGB(x,y), true);
-				Color dest = source.darker();
-				m.setRGB(x,y,dest.getRGB());//TODO:WritableRaster
-			}
-			int stepRed = (255-red)/HALF_HDP;//256做被减数颜色会抖动
-			if(DEBUG)System.err.print(stepRed+"#");
-			step[i][j] = 0x10000*stepRed;
+			step[i][j] = handleTooBright(m,x,y); 
 		}
 	}
 	//循环写入
 	BufferedImage[] frames = new BufferedImage[HALF_HDP*2];
 	frames[0] = m;	//首帧使用（可能被 sToStep 修改过的）m 
-//闪烁深度 ( 以 HALF_HDP==6 , step==10时为例 ) 
-//frames下标：  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11。（0为原图）
-//红分量深度：100,110,120,130,140,150,160,150,140,130,120,110。（100为原图深度）
+/* 三重循环算出半程帧并引用于后半程 ( 以 HALF_HDP==6 , step==30时为例 ) 
+ * frames下标： 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10,11（0为原图）
+ * 红分量深度：30,40,50,60,70,80,90,80,70,60,50,40（100为原图深度）
+ */
 	for(int k=1; k <= HALF_HDP ;k++){ 
 		frames[k] = nextFrame(imClone(frames[k-1]), step, rb, cb, 1);
 		frames[frames.length-k] = frames[k]; //往返闪烁
 	}
+	writeFrames(frames); //REPEAT次一重循环写入帧
+    }
+    public static int handleTooBright(BufferedImage m, int x, int y){
+	    int red;//处理过亮的像素
+	    while( ( red = rgba2Red(m.getRGB(x,y)) ) > MAX_SUB_RED){
+		    Color source = new Color(m.getRGB(x,y), true);
+		    Color dest = source.darker();
+		    m.setRGB(x,y,dest.getRGB());//TODO:WritableRaster
+	    }
+	    int stepRed = (255-red)/HALF_HDP;//256做被减数颜色会抖动
+	    if(DEBUG)System.err.print(stepRed+"#");
+	    return 0x10000*stepRed;
+    }
+    public static void writeFrames(BufferedImage[] frames){
 	for(int t=0;t<REPEAT;++t){
 		for(int f=0;f<frames.length;++f)
-			ImageIO.write(frames[f], FRAME_FORMAT, sOut());
+			try{
+				ImageIO.write(frames[f], FRAME_FORMAT, sOut());
+			}catch(IOException ex){
+				throw new AssertionError();
+			}
 		if(F_OUT_DEBUG) {
 			System.err.println("文件DEBUG状态下只需写盘一轮"); 
 			break;
@@ -122,10 +136,5 @@ public class MS2Frame{
 		}
 	}
 	return m;
-    }
-    public static BufferedImage imClone(BufferedImage from){
-	BufferedImage to = new BufferedImage(from.getWidth(), from.getHeight(), from.getType());
-        to.setData(from.getData());
-	return to;
     }
 }

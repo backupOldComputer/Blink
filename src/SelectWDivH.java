d1bcfa8ff4b8b40980e83ec16377cee3feeb78a4
钟本聪 <641045726@qq.com>
2020-12-21 10:31:28 +0800

    重构src/SelectWDivH.java

--- src/SelectWDivH.java
+++ src/SelectWDivH.java
@@ -4,6 +4,7 @@ import java.awt.image.*;
 import javax.imageio.*;
 
 public class SelectWDivH{
+    public static final String SUFFIX = "png";
     public static final int TARGET = 150 ; // 66 ; // 对大部分相机而言，纵向图宽高比约为0.66;
     public static final int BOX = 100;
     private static int[] divCount = new int[BOX*3+1];
@@ -16,18 +17,17 @@ public class SelectWDivH{
 */
 	for(int i=0;i<args.length;++i){
 		BufferedImage m = ImageIO.read(new FileInputStream(args[i]));
-		double len = (double)BOX;
-		double folder = m.getWidth() * len /m.getHeight();
+		double folder = ( m.getWidth() * (double)BOX ) /m.getHeight();
 		int lenMul = (int)folder;
 		divCount[lenMul] += 1;
 		
 		if( lenMul==TARGET )
-			ImageIO.write(m, "png", new File(args[i]+".png"));
+			ImageIO.write(m, SUFFIX, new File(args[i]+"."+SUFFIX));
 		
-		if(i%100==0) System.out.println("还剩"+(args.length-i)); 
+		if(i%100==0) System.out.print("还剩"+(args.length-i)); 
 	}
-	System.out.println("66----------"+divCount[66]);
-	System.out.println("150---------"+divCount[150]);
+	System.out.print("\n divCount[66] =="+divCount[66]);
+	System.out.print("\n divCount[150]=="+divCount[150]);
 	showCount();
     }
     public static void showCount() {
